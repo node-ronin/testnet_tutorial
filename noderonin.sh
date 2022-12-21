c3dfc6a2ee10c6dcbc4886dd48532f8050c5c94d
alteregogi <ginanjar.lillunatic@gmail.com>
2022-12-21 20:29:50 +0700

    Update noderonin.sh

--- noderonin.sh
+++ noderonin.sh
@@ -36,5 +36,6 @@ StepBox "
                         ██║░░██║╚█████╔╝██║░╚███║██║██║░╚███║
                         ╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝╚═╝░░╚══╝
 
-                    Join Discord: https://discord.gg/YqVrXbHTBU
+                    Join Discord    : https://discord.gg/YqVrXbHTBU
+                    Github Page     : https://github.com/node-ronin
 " 80 "="
